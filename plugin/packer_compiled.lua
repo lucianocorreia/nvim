771118a2f33cc042c0931d5422b5f8f3b8a78e94
Luciano Correia <luciano.correia@gmail.com>
2022-09-30 16:40:55 -0300

    fix lsp config

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -79,6 +79,11 @@ _G.packer_plugins = {
     path = "C:\\Users\\Luciano Correia\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\LuaSnip",
     url = "https://github.com/L3MON4D3/LuaSnip"
   },
+  catppuccin = {
+    loaded = true,
+    path = "C:\\Users\\Luciano Correia\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\catppuccin",
+    url = "https://github.com/catppuccin/nvim"
+  },
   ["cmp-buffer"] = {
     loaded = true,
     path = "C:\\Users\\Luciano Correia\\AppData\\Local\\nvim-data\\site\\pack\\packer\\start\\cmp-buffer",
