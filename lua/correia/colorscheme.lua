b81c4d7c2fcb7212b90b511e8bac073433691d4d
Luciano Correia <luciano.correia@gmail.com>
2022-11-17 22:27:04 +0400

    update config

--- lua/correia/colorscheme.lua
+++ lua/correia/colorscheme.lua
@@ -17,44 +17,45 @@
 -- vim.cmd [[colorscheme tokyonight]]
 vim.cmd [[colorscheme catppuccin-macchiato]]
 
-require("catppuccin").setup({
-    flavour = "macchiato", -- latte, frappe, macchiato, mocha
-    background = { -- :h background
-        light = "latte",
-        dark = "macchiato",
-    },
-    compile_path = vim.fn.stdpath("cache") .. "/catppuccin",
-    transparent_background = false,
-    term_colors = false,
-    dim_inactive = {
-        enabled = false,
-        shade = "dark",
-        percentage = 0.15,
-    },
-    styles = {
-        comments = { "italic" },
-        conditionals = { "italic" },
-        loops = {},
-        functions = {},
-        keywords = {},
-        strings = {},
-        variables = {},
-        numbers = {},
-        booleans = {},
-        properties = {},
-        types = {},
-        operators = {},
-    },
-    color_overrides = {},
-    custom_highlights = {},
-    integrations = {
-        cmp = true,
-        gitsigns = true,
-        nvimtree = true,
-        telescope = true,
-        treesitter = true,
-        mason = true,
-        dashboard = true,
-        -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
-    },
-})
+require("catppuccin").setup({})
+-- require("catppuccin").setup({
+--     flavour = "macchiato", -- latte, frappe, macchiato, mocha
+--     background = { -- :h background
+--         light = "latte",
+--         dark = "macchiato",
+--     },
+--     compile_path = vim.fn.stdpath("cache") .. "/catppuccin",
+--     transparent_background = false,
+--     term_colors = false,
+--     dim_inactive = {
+--         enabled = false,
+--         shade = "dark",
+--         percentage = 0.15,
+--     },
+--     styles = {
+--         comments = { "italic" },
+--         conditionals = { "italic" },
+--         loops = {},
+--         functions = {},
+--         keywords = {},
+--         strings = {},
+--         variables = {},
+--         numbers = {},
+--         booleans = {},
+--         properties = {},
+--         types = {},
+--         operators = {},
+--     },
+--     color_overrides = {},
+--     custom_highlights = {},
+--     integrations = {
+--         cmp = true,
+--         gitsigns = true,
+--         nvimtree = true,
+--         telescope = true,
+--         treesitter = true,
+--         mason = true,
+--         dashboard = true,
+--         -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+--     },
+-- })
