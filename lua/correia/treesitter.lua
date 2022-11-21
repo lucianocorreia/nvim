47b7af4c333f43834dac2ec4a2dad0f1a703c591
Luciano Correia <luciano.correia@gmail.com>
2022-11-21 16:19:35 +0400

    update config

--- lua/correia/treesitter.lua
+++ lua/correia/treesitter.lua
@@ -35,4 +35,15 @@ local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
 parser_config.tsx.filetype_to_parsername = { "javascript", "typescript.tsx" }
 
 
-require 'treesitter-context'.setup {}
+require 'treesitter-context'.setup {
+    patterns = { -- Match patterns for TS nodes. These get wrapped to match at word boundaries.
+        -- For all filetypes
+        -- Note that setting an entry here replaces all other patterns for this entry.
+        -- By setting the 'default' entry below, you can control which nodes you want to
+        -- appear in the context window.
+        default = {
+            'function',
+            'method',
+        }
+    }
+}
