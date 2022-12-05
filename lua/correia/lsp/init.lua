f0b4cf91152e78979b8471a93044d0235d9cf1d3
Luciano Correia <luciano.correia@gmail.com>
2022-12-05 13:37:35 +0400

    update condig

--- lua/correia/lsp/init.lua
+++ lua/correia/lsp/init.lua
@@ -7,7 +7,8 @@ require("correia.lsp.handlers").setup()
 
 local lspconfig = require("lspconfig")
 
-local servers = { "jsonls", "sumneko_lua", "intelephense", "tsserver", "rust_analyzer", "vuels", "tailwindcss", "gopls", "sqls", "bufls", "dockerls"}
+local servers = { "jsonls", "sumneko_lua", "intelephense", "tsserver", "rust_analyzer", "vuels", "tailwindcss", "gopls",
+    "sqls", "bufls", "dockerls", "yamlls" }
 
 for _, server in pairs(servers) do
     local opts = {
